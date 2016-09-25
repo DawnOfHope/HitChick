a585962057e7a600b1dc1dc0f917ec660277235d
DawnOfHope <DawnOfHope.zora@gmail.com>
2016-09-25 15:28:51 +0800

    全螢幕

--- app/src/main/java/com/example/zorahu/hitchick/MainActivity.java
+++ app/src/main/java/com/example/zorahu/hitchick/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.zorahu.hitchick;
 
+import android.os.Handler;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageViewList;
+    private TextView scoreBar;
+    private int[] manychick;
+    private boolean play;
+    private Handler handler;
+    private int score;
+    private ChickSprite[] chickSprites;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        int num1;
+        num1 = (int)(Math.random()*10)+1;
+
+        imageViewList = (ImageView) findViewById(R.id.hole);
+
     }
+
+    private class ChickSprite implements Runnable{
+        @Override
+        public void run() {
+
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/zorahu/hitchick/WelcomeActivity.java
+++ app/src/main/java/com/example/zorahu/hitchick/WelcomeActivity.java
@@ -5,6 +5,8 @@ import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 
@@ -14,6 +16,9 @@ public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_welcome);
 
         btnbanner1 = (ImageButton) findViewById(R.id.btnBanner1);
