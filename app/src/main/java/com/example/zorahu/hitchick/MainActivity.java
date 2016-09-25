de616301bc1e9ae3e74ab52e6cde550597edf47e
DawnOfHope <DawnOfHope.zora@gmail.com>
2016-09-25 16:35:26 +0800

    小雞亂數產生

--- app/src/main/java/com/example/zorahu/hitchick/MainActivity.java
+++ app/src/main/java/com/example/zorahu/hitchick/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
@@ -12,7 +14,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView imageViewList;
+    private ImageView[] imageViewList;
     private TextView scoreBar;
     private int[] manychick;
     private boolean play;
@@ -29,17 +31,90 @@ public class MainActivity extends AppCompatActivity {
         WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        scoreBar = (TextView) findViewById(R.id.scoreBar);
+        //建立hole陣列
+        imageViewList = new ImageView[]{
+                (ImageView) findViewById(R.id.hole),
+                (ImageView) findViewById(R.id.hole2),
+                (ImageView) findViewById(R.id.hole3),
+                (ImageView) findViewById(R.id.hole4),
+                (ImageView) findViewById(R.id.hole5),
+                (ImageView) findViewById(R.id.hole6),
+                (ImageView) findViewById(R.id.hole7),
+                (ImageView) findViewById(R.id.hole8),
+                (ImageView) findViewById(R.id.hole9),
+        };
 
-        imageViewList = (ImageView) findViewById(R.id.hole);
+        //建立小雞陣列
+        manychick = new int[]{
+            R.drawable.hole,R.drawable.j2,R.drawable.j3,R.drawable.j4,
+                R.drawable.j5,R.drawable.j6,R.drawable.j7,R.drawable.j8
+        };
 
+        handler = new Handler();
+
+        chickSprites = new ChickSprite[9];
+        for (int i= 0; i < chickSprites.length ; i++){
+            chickSprites[i] = new ChickSprite(imageViewList[i]);
+            imageViewList[i].setOnTouchListener(new ChickOnTouchLister(chickSprites[i]));
+        }
     }
 
     private class ChickSprite implements Runnable{
+        ImageView imageView;
+        int idx;
+        boolean hit;
+        ChickSprite(ImageView imageView){
+            this.imageView = imageView;
+        }
         @Override
         public void run() {
+            draw();
+        }
+        private void draw(){
+            if (!play){
+                return;
+            }
+            //點擊發生
+            if (hit){
+                imageView.setImageResource(R.drawable.dg);//變更成傷害圖片
+                hit = false;//還原點擊屬性
+                idx = 0;//調整陣列維度
+                handler.postDelayed(this,5000);//每次遞迴延遲秒數
 
+            }else {
+                idx = idx % manychick.length;
+                imageView.setImageResource(manychick[idx]);
+                int n = (int)(Math.random()*1000)%3+1;
+                handler.postDelayed(this,(n*300));//遞迴呼叫秒數
+                idx = ++idx % manychick.length;
+            }
         }
 
+    }
+
+    private class ChickOnTouchLister implements View.OnTouchListener{
+        ChickSprite bobogi;
+        ChickOnTouchLister(ChickSprite bobogi){
+            this.bobogi = bobogi;
+        }
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            if (play && event.getAction() == MotionEvent.ACTION_DOWN){
+                if (manychick[bobogi.idx] == R.drawable.j4 ||
+                        manychick[bobogi.idx] == R.drawable.j5 ||
+                        manychick[bobogi.idx] == R.drawable.j6 ||
+                        manychick[bobogi.idx] == R.drawable.j7){
+                    bobogi.hit = true;
+                    scoreBar.setText(String.valueOf(++score));
+
+                }else {
+                    scoreBar.setText(String.valueOf(--score));
+                }
+            }
+            return false;
+        }
     }
 
 }
