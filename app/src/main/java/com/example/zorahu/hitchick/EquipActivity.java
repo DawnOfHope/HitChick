7fe44c288ff9a64d51616cac923af82c5431c1bf
DawnOfHope <DawnOfHope.zora@gmail.com>
2016-09-26 13:02:04 +0800

    add 裝備介面返回功能

--- app/src/main/java/com/example/zorahu/hitchick/EquipActivity.java
+++ app/src/main/java/com/example/zorahu/hitchick/EquipActivity.java
@@ -1,11 +1,15 @@
 package com.example.zorahu.hitchick;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageButton;
 
 public class EquipActivity extends AppCompatActivity {
+    private ImageButton btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +18,20 @@ public class EquipActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_equipe);
+
+        btnBack = (ImageButton) findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                btnBack.setBackgroundResource(R.drawable.k2a);
+                goWelcome();
+            }
+        });
+    }
+
+    private void goWelcome(){
+        Intent intent = new Intent(this,WelcomeActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
